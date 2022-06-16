f26dce5e35a0aed2cb1d519dd4435b91d4922256
kul2022 <107618440+kul2022@users.noreply.github.com>
2022-06-16 15:38:13 +0530

    check_Triangle.py
    Triangle  checking

---
+++ check_Triangle.py
@@ -0,0 +1,7 @@
+num1 = int(input('Enter a angle 1:- '))
+num2 = int(input('Enter a angle 2:- '))
+num3 = int(input('Enter a angle 3:- '))
+if num1 + num2 + num3 == 180:
+    print('It is Triangle')
+else:
+    print('It is  not Triangle')
